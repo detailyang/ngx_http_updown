e8531e6d075d376f336e04187355acda427e8b69
detailyang <detailyang@gmail.com>
2015-10-24 12:27:27 +0800

    add post delete

--- ngx_http_test_module.c
+++ ngx_http_test_module.c
@@ -2,7 +2,7 @@
 * @Author: detailyang
 * @Date:   2015-10-24 10:36:19
 * @Last Modified by:   detailyang
-* @Last Modified time: 2015-10-24 12:05:16
+* @Last Modified time: 2015-10-24 12:27:09
 */
 
 #include <ngx_config.h>
@@ -50,11 +50,31 @@ static int ngx_updown_status = 0;
 static ngx_int_t handler_get (ngx_http_request_t *req) {
   u_char ngx_response_body[1024] = {0};
 
-  ngx_log_error(NGX_LOG_ERR, req->connection->log, 0, "receive request");
+  if (ngx_updown_status == 0) {
+    ngx_sprintf(ngx_response_body, "down");
+    req->headers_out.status = 500;
+  } else {
+    ngx_sprintf(ngx_response_body, "up");
+    req->headers_out.status = 200;
+  }
+  req->headers_out.content_length_n = ngx_strlen(ngx_response_body);;
+  ngx_str_set(&req->headers_out.content_type, "text/html");
+  ngx_http_send_header(req);
 
-  ngx_log_error(NGX_LOG_ERR, req->connection->log, 0,
-                          "url:%V method:%V",
-                          &req->uri, &req->method_name);
+  ngx_buf_t *b; b = ngx_pcalloc(req->pool, sizeof(ngx_buf_t));
+  ngx_chain_t out;
+  out.buf = b;
+  out.next = NULL;
+  b->pos = ngx_response_body;
+  b->last = ngx_response_body + req->headers_out.content_length_n;
+  b->memory = 1;
+  b->last_buf = 1;
+
+  return ngx_http_output_filter(req, &out);
+}
+
+static ngx_int_t handler_get(ngx_http_request_t *req) {
+  u_char ngx_response_body[1024] = {0};
 
   if (ngx_updown_status == 0) {
     ngx_sprintf(ngx_response_body, "down");
@@ -79,14 +99,56 @@ static ngx_int_t handler_get (ngx_http_request_t *req) {
   return ngx_http_output_filter(req, &out);
 }
 
+static ngx_int_t handler_post(ngx_http_request_t *req) {
+  u_char ngx_response_body[1024] = {0};
+
+  ngx_updown_status = 1;
+  req->headers_out.content_length_n = ngx_strlen(ngx_response_body);;
+  req->headers_out.status = 200;
+  ngx_str_set(&req->headers_out.content_type, "text/html");
+  ngx_http_send_header(req);
+
+  ngx_buf_t *b; b = ngx_pcalloc(req->pool, sizeof(ngx_buf_t));
+  ngx_chain_t out;
+  out.buf = b;
+  out.next = NULL;
+  b->pos = ngx_response_body;
+  b->last = ngx_response_body + req->headers_out.content_length_n;
+  b->memory = 1;
+  b->last_buf = 1;
+
+  return ngx_http_output_filter(req, &out);
+}
+
+static ngx_int_t handler_delete(ngx_http_request_t *req) {
+  u_char ngx_response_body[1024] = {0};
+
+  ngx_updown_status = 0;
+  req->headers_out.content_length_n = ngx_strlen(ngx_response_body);;
+  req->headers_out.status = 200;
+  ngx_str_set(&req->headers_out.content_type, "text/html");
+  ngx_http_send_header(req);
+
+  ngx_buf_t *b; b = ngx_pcalloc(req->pool, sizeof(ngx_buf_t));
+  ngx_chain_t out;
+  out.buf = b;
+  out.next = NULL;
+  b->pos = ngx_response_body;
+  b->last = ngx_response_body + req->headers_out.content_length_n;
+  b->memory = 1;
+  b->last_buf = 1;
+
+  return ngx_http_output_filter(req, &out);
+}
+
 static ngx_int_t handler(ngx_http_request_t *req) {
   switch(req->method) {
     case NGX_HTTP_GET:
       return handler_get(req);
-    // case NGX_HTTP_POST:
-    //   return post(req);
-    // case NGX_HTTP_DELETE:
-    //   return delete(req);
+    case NGX_HTTP_POST:
+      return post(req);
+    case NGX_HTTP_DELETE:
+      return delete(req);
     default:
       return handler_get(req);
   }
